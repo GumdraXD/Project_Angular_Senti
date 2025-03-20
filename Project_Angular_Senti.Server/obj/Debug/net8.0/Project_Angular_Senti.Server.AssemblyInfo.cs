571cf0525c25b9dfed81e74fbad4bb45a1a1345d
GumdraXD <gumdraxd@gmail.com>
2025-03-19 20:41:02 -0700

    CSV File (Needs values seperated into different columns)

--- Project_Angular_Senti.Server/obj/Debug/net8.0/Project_Angular_Senti.Server.AssemblyInfo.cs
+++ Project_Angular_Senti.Server/obj/Debug/net8.0/Project_Angular_Senti.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_Angular_Senti.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fc1eb7dc4c7aeb736f272e9efdc6eea62fdea20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bc370f74e74bf1d3db5d2fdc7fbf97dc574421b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_Angular_Senti.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_Angular_Senti.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
